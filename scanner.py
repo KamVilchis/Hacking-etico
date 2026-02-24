f4cd42d4a897ef6008bed8925a8324ae969e7e6b
KamVilchis <79608550+KamVilchis@users.noreply.github.com>
2026-02-24 17:41:59 -0600

    Fix indentation and formatting in scanner.py

--- scanner.py
+++ scanner.py
@@ -10,19 +10,16 @@ for puerto in range(puerto_inicio, puerto_fin + 1):
         socket_conexion.settimeout(3)
 
         resultado = socket_conexion.connect_ex((ip_objetivo, puerto))
+        
         if resultado == 0:
             print(f"[+] Puerto {puerto}: ABIERTO")
-
             try:
                 banner = socket_conexion.recv(1024)
                 banner_texto = banner.decode('utf-8', errors='ignore')
-
-
                 if banner_texto:
                         print(f"    ↳ Banner: {banner_texto}")
                 else:
                     print("    ↳ Banner: No recibido")
-
             except:
                 print("    ↳ Banner: No se pudo capturar")
 
